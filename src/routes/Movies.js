269844d5dc9f3ae81abbd026c6424c5dbe295efb
RoJaebl <qgs0924@gmail.com>
2022-11-03 23:00:18 +0900

    :hammer: useQuery feature

--- src/routes/Movies.js
+++ src/routes/Movies.js
@@ -1,26 +1,26 @@
-import { useApolloClient, gql } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIES = gql`
+  query getMovies {
+    allMovies {
+      title
+      id
+    }
+  }
+`;
 
 function Movies() {
-  const [movies, setMovies] = useState([]);
-  const client = useApolloClient();
-  useEffect(() => {
-    client
-      .query({
-        query: gql`
-          {
-            allMovies {
-              id
-              title
-            }
-          }
-        `,
-      })
-      .then((results) => setMovies(results.data.allMovies));
-  }, [client]);
+  const { data, loading, error } = useQuery(GET_MOVIES);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) {
+    return <h1>Could not fetch :(</h1>;
+  }
   return (
     <ul>
-      {movies.map((movie) => (
+      <h1>Movies</h1>
+      {data.allMovies.map((movie) => (
         <li key={movie.id}>{movie.title}</li>
       ))}
     </ul>
